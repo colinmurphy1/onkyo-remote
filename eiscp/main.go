87c0b3dd402ae48ac256b0051b2932bf03bf5e1a
Colin Murphy <colin@colinmurphy.me>
2022-06-17 18:41:34 -0500

    Run QSTN commands on startup to prepopulate the OnkyoStatus struct

--- eiscp/main.go
+++ eiscp/main.go
@@ -1,6 +1,10 @@
 package eiscp
 
-import "github.com/colinmurphy1/onkyo-remote/config"
+import (
+	"log"
+
+	"github.com/colinmurphy1/onkyo-remote/config"
+)
 
 // Connection struct is stored here for access outside of this package
 var Conn *Connection
@@ -12,9 +16,18 @@ func init() {
 	// Start command watcher goroutine
 	go Conn.EiscpWatcher()
 
-	// Initialize Connection.Status struct
-	Conn.SendCmd("PWRQSTN")
-	Conn.SendCmd("MVLQSTN")
-	Conn.SendCmd("AMTQSTN")
-	Conn.SendCmd("SLIQSTN")
+	// Initialize OnkyoStatus struct by running QSTN commands on power status,
+	// volume level, input, song information, etc...
+	// NOTE: It does not matter if you run a QSTN command with the receiver
+	// powered on or off, it'll answer with what it has stored.
+	startCommands := []string{
+		"PWRQSTN", "MVLQSTN", "AMTQSTN", "SLIQSTN", "PRSQSTN", "TUNQSTN",
+		"NTIQSTN", "NATQSTN", "NALQSTN", "NTRQSTN",
+	}
+	for i := 0; i < len(startCommands); i++ {
+		err := Conn.SendCmd(startCommands[i])
+		if err != nil {
+			log.Println("Error running startup command:", err)
+		}
+	}
 }
