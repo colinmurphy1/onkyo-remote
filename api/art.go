992d4f735855cc94b61bfa28cfd96a6cb929b494
Colin Murphy <colin@colinmurphy.me>
2022-06-19 21:37:11 -0500

    Use a 1x1 pixel in place of album art if none is available

--- api/art.go
+++ api/art.go
@@ -1,14 +1,22 @@
 package api
 
 import (
+	"encoding/base64"
+
 	"github.com/colinmurphy1/onkyo-remote/eiscp"
 	"github.com/gin-gonic/gin"
 )
 
+// https://png-pixel.com/
+var pixel string = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+
 func GetArt(c *gin.Context) {
 
+	// Decode base64 encoded pixel image
+	x, _ := base64.StdEncoding.DecodeString(pixel)
+
 	if len(eiscp.Conn.AlbumArt.Data) == 0 {
-		c.Data(503, "text/plain", nil)
+		c.Data(200, "image/png", x)
 		return
 	}
 
