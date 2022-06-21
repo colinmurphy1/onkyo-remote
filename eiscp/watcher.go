5f655b5b83671d5060daeb4ea27bd769ef76ed08
Colin Murphy <colin@colinmurphy.me>
2022-06-20 21:31:07 -0500

    Update gitignore

--- eiscp/watcher.go
+++ eiscp/watcher.go
@@ -17,7 +17,7 @@ func (c *Connection) EiscpWatcher() {
 		response, err = c.RecvCmd() // Receive command from the receiver
 		if err != nil {
 			log.Println("RECV ERROR:", err)
-			continue // Recover from any errors
+			break // End the loop
 		}
 
 		cmd = response[2:5]     // iscp command
